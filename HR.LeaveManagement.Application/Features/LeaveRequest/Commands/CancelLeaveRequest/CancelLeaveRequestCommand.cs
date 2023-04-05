f9a8ecd0c204f6168fe3e0bc5911c3fb60312a69
maloney_f <missymaloney1@gmail.com>
2023-04-05 11:59:20 -0400

    Added LeaveAllocation and LeaveRequest Features

---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CancelLeaveRequest/CancelLeaveRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CancelLeaveRequest
+{
+    public class CancelLeaveRequestCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Commands/CreateLeaveRequest/CreateLeaveRequestCommand.cs
@@ -0,0 +1,11 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveRequest.Shared;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequest.Commands.CreateLeaveRequest
+{
+    public class CreateLeaveRequestCommand : BaseLeaveRequest, IRequest<Unit>
+    {
+        public string RequestComments { get; set; } = string.Empty;
+    }
+}
+
