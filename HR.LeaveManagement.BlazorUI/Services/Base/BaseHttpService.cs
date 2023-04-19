5a6107e11325ca7b37ac2d6f459d3abf5bf5f55f
maloney_f <missymaloney1@gmail.com>
2023-04-19 14:40:11 -0400

    added nswag and http extensions (lesson 59)

---
+++ HR.LeaveManagement.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,12 @@
+﻿namespace HR.LeaveManagement.BlazorUI.Services.Base
+{
+    public class BaseHttpService
+    {
+        protected IClient _client;
+
+        public BaseHttpService(IClient client)
+        {
+            _client = client;
+        }
+    }
+}
